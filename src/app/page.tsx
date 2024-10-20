9b80c202ca755f90fb9aa81b41296422983cc5a7
jinpengwu <jinpengwu68@gmail.com>
2024-10-21 05:46:27 +1100

    Tailwind using

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      Root Page <Button>Click me</Button>
+    <div className="h-screen flex justify-center items-center">
+      <form className="w-full max-w-md flex-col gap-4">
+        <h1 className="text-center text-2xl font-bold">Create App</h1>
+        <Input name="name" placeholder="App name"></Input>
+        <Textarea name="description" placeholder="Description"></Textarea>
+        <Button type="submit">Submit</Button>
+      </form>
     </div>
   );
 }
